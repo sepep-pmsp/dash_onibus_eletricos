c081986a7847ad3027df01673fce72bcdaac32ea
Caio Durazzo <caio.durazzo220704@gmail.com>
2025-08-21 10:54:24 -0300

    dash

--- app.py
+++ app.py
@@ -369,7 +369,7 @@ with col_center:
             current_time=current_time,
         )
         view_state = pdk.ViewState(latitude=-23.6, longitude=-46.63, zoom=11, pitch=45)
-        r = pdk.Deck(layers=[trips_layer], initial_view_state=view_state, map_style="mapbox://styles/mapbox/light-v11")
+        r = pdk.Deck(layers=[trips_layer], initial_view_state=view_state)
         map_placeholder.pydeck_chart(r, height=600)
         current_time += time_step
         time.sleep(frame_delay)
